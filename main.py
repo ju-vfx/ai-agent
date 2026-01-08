193688e4e07528d5eb2f774e26ba37165734c4ca
Julian Utz <69083358+ju-vfx@users.noreply.github.com>
2026-01-08 20:36:05 +0100

    added google gemini

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
+
+def main():
+    client = genai.Client(api_key=api_key)
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    print(response.text)
 
 if __name__ == "__main__":
     main()
